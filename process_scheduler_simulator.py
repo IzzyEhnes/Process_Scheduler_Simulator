8e0f336e0cdba56ccb6baa29e3f8c0078b855fe7
Izzy Ehnes <izzyehnes@gmail.com>
2022-04-12 22:49:21 -0700

    Add statistics variable and compute_wait_times

--- process_scheduler_simulator.py
+++ process_scheduler_simulator.py
@@ -2,6 +2,26 @@ import sys
 
 # Parent class
 class OS:
+    # wait time per process, turn around time per process, throughput
+    statistics = [[], [], 0.0]
+
+    def get_user_input():
+        print("Enter triples: process id, time in ms, and priority (Ctrl-D to end input):")
+        print("For example: \n1 12 0\n3  9 1\n2 99 9")
+        print("process 1 needs 12 ms and has priority 0, very high,")
+        print("process 3 needs  9 ms and has priority 1.")
+        print("and so on ...")
+
+        content = sys.stdin.read()
+        content = " ".join(content.split()).split()
+
+        triples = list()
+        while content:
+            triples.append(content[:3])
+            content = content[3:]
+
+        OS.process_list = triples
+
     def print_process_list():
         for line in OS.process_list:
             print(' '.join(line))
@@ -10,7 +30,13 @@ class OS:
 
 # First Come First Serve class; child of OS class
 class FCFS(OS):
-    pass
+    def compute_wait_times():
+        wait_time = 0
+        for process in FCFS.process_list:
+            FCFS.statistics[0].append(wait_time)
+            wait_time += int(process[1])
+
+        print(FCFS.statistics)
 
 
 
@@ -27,26 +53,17 @@ class RR(OS):
 
 
 
-def get_user_input():
-    print("Enter triples: process id, time in ms, and priority (Ctrl-D to end input):")
-    print("For example: \n1 12 0\n3  9 1\n2 99 9")
-    print("process 1 needs 12 ms and has priority 0, very high,")
-    print("process 3 needs  9 ms and has priority 1.")
-    print("and so on ...")
+def run_simulation():
+    OS.get_user_input()
+    print()
 
-    content = sys.stdin.read()
-    content = " ".join(content.split()).split()
+    print("Process list in FCFS order as entered:")
+    FCFS.print_process_list()
 
-    triples = list()
-    while content:
-        triples.append(content[:3])
-        content = content[3:]
+    print("")
 
-    OS.process_list = triples
+    FCFS.compute_wait_times()
 
 
 
-get_user_input()
-print()
-HPF.sort_list()
-HPF.print_process_list()
+run_simulation()
